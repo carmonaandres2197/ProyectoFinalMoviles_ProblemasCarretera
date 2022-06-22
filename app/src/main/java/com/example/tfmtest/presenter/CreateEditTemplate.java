e24a54396b32243918c6725687931a7e0fa65054
CruzStark <christiandacruz04@gmail.com>
2022-06-22 00:26:46 -0600

    updating ui pantalla 2, fotos desde celular y video

--- app/src/main/java/com/example/tfmtest/presenter/CreateEditTemplate.java
+++ app/src/main/java/com/example/tfmtest/presenter/CreateEditTemplate.java
@@ -1,16 +1,102 @@
 package com.example.tfmtest.presenter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.example.tfmtest.R;
 
-public class CreateEditTemplate extends AppCompatActivity {
+public class CreateEditTemplate  extends AppCompatActivity {
+    Button btnTakePhoto;
+    Button btnTakeVideo;
+
+    ImageView imageView;
+    VideoView videoView;
+    public static final int RequestPermissionCode = 1;
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_edit_template);
+
+        btnTakePhoto = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+        videoView = findViewById((R.id.simpleVideoView));
+        EnableRuntimePermission();
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 7);
+            }
+        });
+
+
+
+        btnTakeVideo = findViewById(R.id.buttonVideo);
+        btnTakeVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+                }
+
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 7 && resultCode == RESULT_OK) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+            videoView.setVideoURI(videoUri);
+        }
+
+    }
+    public void EnableRuntimePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(CreateEditTemplate.this,
+                Manifest.permission.CAMERA)) {
+            Toast.makeText(CreateEditTemplate.this,"CAMERA permission allows us to Access CAMERA app",     Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(CreateEditTemplate.this,new String[]{
+                    Manifest.permission.CAMERA}, RequestPermissionCode);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] result) {
+        super.onRequestPermissionsResult(requestCode, permissions, result);
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (result.length > 0 && result[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(CreateEditTemplate.this, "Permission Granted, Now your application can access CAMERA.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(CreateEditTemplate.this, "Permission Canceled, Now your application cannot access CAMERA.", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 }
