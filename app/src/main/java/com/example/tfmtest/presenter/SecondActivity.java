95f31e390cf9184fb7f89c56f574040d6b4fc503
lawny04 <lawny.vargas@gmail.com>
2022-06-24 23:20:34 -0600

    Update CreateEditTemplate.java
    spinners

--- app/src/main/java/com/example/tfmtest/presenter/SecondActivity.java
+++ app/src/main/java/com/example/tfmtest/presenter/SecondActivity.java
@@ -1,26 +1,18 @@
 package com.example.tfmtest.presenter;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.tfmtest.adapters.ReportsListAdapter;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tfmtest.R;
-import com.example.tfmtest.database.DataBase;
-import com.example.tfmtest.interfaces.Callback;
-import com.example.tfmtest.interfaces.RealtimeDataListener;
+import com.example.tfmtest.adapters.ReportsListAdapter;
 import com.example.tfmtest.model.Reporte;
-import com.example.tfmtest.utils.Loading;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -28,21 +20,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 
