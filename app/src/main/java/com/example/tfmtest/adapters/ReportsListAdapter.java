0ae9e73b124daca4d368a6eea5577cda44652b11
ScarlethSalinas <scarlethsg.23@gmail.com>
2022-06-26 13:50:24 -0600

    carga de datos de la base en la pantalla 1

--- app/src/main/java/com/example/tfmtest/adapters/ReportsListAdapter.java
+++ app/src/main/java/com/example/tfmtest/adapters/ReportsListAdapter.java
@@ -1,11 +1,5 @@
 package com.example.tfmtest.adapters;
 
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,48 +7,30 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tfmtest.R;
 import com.example.tfmtest.model.Reporte;
-import com.example.tfmtest.presenter.SecondActivity;
-import com.example.tfmtest.presenter.TabActivity;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
-import java.util.Date;
-import java.util.List;
+public class ReportsListAdapter extends FirestoreRecyclerAdapter<Reporte,ReportsListAdapter.ReportViewHolder>{
 
-public class ReportsListAdapter extends RecyclerView.Adapter<ReportsListAdapter.ReportViewHolder> {
-    Context  context;
-    private List<Reporte> mReports; // Cached copy of words
-
-    String[] nombres = {"Cobano, Pun, 100 mts sur cancha", "Mts Oca, SJ, Frente a Iglesia", "Cahuita, Lim, 50 mts este del parque", "Caldera, SJ, ruta nacional 707","Puerto Viejo, Lim, 150 mts este de la principal","Turrialba, Cartago, 100 mts sur de la Ruta 10"};
-    Date[] fechas = {new Date(), new Date(), new Date(), new Date(), new Date(), new Date()};
-    String[] severidad = {"Baja", "Media", "Alta", "Alta", "Alta", "Baja"};
-    Boolean[] estado = {true, false, true, false, true, false};
-
-    public ReportsListAdapter(Context context) {
-        //this.mReports = reportes;
-        this.context = context;
-        notifyDataSetChanged();
-
+    public ReportsListAdapter(@NonNull FirestoreRecyclerOptions<Reporte> options) {
+        super(options);
     }
 
 
-    @NonNull
-    @Override
-    public ReportViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View item = LayoutInflater.from(context).inflate(R.layout.item_report_dh, parent, false);
 
-        return new ReportViewHolder(item);
-    }
 
     @Override
-    public void onBindViewHolder(@NonNull ReportsListAdapter.ReportViewHolder holder, int position) {
-        //Reporte report = mReports.get(position);
-        holder.nombreHueco.setText(nombres[position]);
-        holder.severidad.setText(severidad[position]);
+    protected void onBindViewHolder(@NonNull ReportViewHolder holder, int position, @NonNull Reporte report) {
 
-        holder.fecha.setText(fechas.toString());
-        holder.estado.setText(estado.toString());
+        holder.nombreHueco.setText(report.getNombre());
+        holder.severidad.setText(report.getSeveridad());
+        holder.fecha.setText(report.getFecha().toString());
+        holder.estado.setText(report.getEstado().toString());
 
         holder.linearLayout.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -81,34 +57,23 @@ public class ReportsListAdapter extends RecyclerView.Adapter<ReportsListAdapter.
         });
 
 
-
-
-      }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
     }
 
+
+    @NonNull
     @Override
-    public int getItemViewType(int position) {
-        return position;
+    public ReportViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_report_dh, parent, false);
+
+        return new ReportViewHolder(v);
     }
 
-    @Override
-    public int getItemCount() { return nombres.length;  }
-
-
-    class ReportViewHolder extends RecyclerView.ViewHolder {
-
+    public class ReportViewHolder extends RecyclerView.ViewHolder {
         public LinearLayout linearLayout;
-        public TextView nombreHueco;
-        public TextView fecha;
-        public TextView severidad;
-        public TextView estado;
+        public TextView nombreHueco, fecha, severidad, estado;
 
 
-        private ReportViewHolder(View itemView) {
+        public ReportViewHolder(@NonNull View itemView) {
             super(itemView);
             linearLayout = itemView.findViewById(R.id.layoutGeneral);
             nombreHueco = itemView.findViewById(R.id.nombree);
@@ -116,7 +81,107 @@ public class ReportsListAdapter extends RecyclerView.Adapter<ReportsListAdapter.
             severidad = itemView.findViewById(R.id.severidad);
             estado = itemView.findViewById(R.id.estado);
 
+
+
         }
     }
+
+
+// RecyclerView.Adapter<ReportsListAdapter.ReportViewHolder>
+//    Context  context;
+//    private List<Reporte> mReports; // Cached copy of words
+//
+//    String[] nombres = {"Cobano, Pun, 100 mts sur cancha", "Mts Oca, SJ, Frente a Iglesia", "Cahuita, Lim, 50 mts este del parque", "Caldera, SJ, ruta nacional 707","Puerto Viejo, Lim, 150 mts este de la principal","Turrialba, Cartago, 100 mts sur de la Ruta 10"};
+//    Date[] fechas = {new Date(), new Date(), new Date(), new Date(), new Date(), new Date()};
+//    String[] severidad = {"Baja", "Media", "Alta", "Alta", "Alta", "Baja"};
+//    Boolean[] estado = {true, false, true, false, true, false};
+//
+//    public ReportsListAdapter(Context context) {
+//        //this.mReports = reportes;
+//        this.context = context;
+//        notifyDataSetChanged();
+//
+//    }
+//
+//
+//    @NonNull
+//    @Override
+//    public ReportViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View item = LayoutInflater.from(context).inflate(R.layout.item_report_dh, parent, false);
+//
+//        return new ReportViewHolder(item);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull ReportsListAdapter.ReportViewHolder holder, int position) {
+//        //Reporte report = mReports.get(position);
+//        holder.nombreHueco.setText(nombres[position]);
+//        holder.severidad.setText(severidad[position]);
+//
+//        holder.fecha.setText(fechas.toString());
+//        holder.estado.setText(estado.toString());
+//
+//        holder.linearLayout.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                v.setOnLongClickListener(this);
+//                Toast.makeText(v.getContext(),"Click on D-H",Toast.LENGTH_LONG).show();
+//              //  nombres.remove(current);
+//                holder.linearLayout.removeViewInLayout(v);
+//
+//                notifyDataSetChanged();
+//                Toast.makeText(
+//                        v.getContext(),
+//                        "D-H Delete Successful",
+//                        Toast.LENGTH_LONG).show();
+//                return false;
+//            }
+//        });
+//
+//        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+//
+//
+//
+//
+//      }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//
+//    @Override
+//    public int getItemViewType(int position) {
+//        return position;
+//    }
+//
+//    @Override
+//    public int getItemCount() { return nombres.length;  }
+//
+//
+//    class ReportViewHolder extends RecyclerView.ViewHolder {
+//
+//        public LinearLayout linearLayout;
+//        public TextView nombreHueco;
+//        public TextView fecha;
+//        public TextView severidad;
+//        public TextView estado;
+//
+//
+//        private ReportViewHolder(View itemView) {
+//            super(itemView);
+//            linearLayout = itemView.findViewById(R.id.layoutGeneral);
+//            nombreHueco = itemView.findViewById(R.id.nombree);
+//            fecha = itemView.findViewById(R.id.fecha);
+//            severidad = itemView.findViewById(R.id.severidad);
+//            estado = itemView.findViewById(R.id.estado);
+//
+//        }
+//    }
 }
 
--- app/src/main/java/com/example/tfmtest/presenter/SecondActivity.java
+++ app/src/main/java/com/example/tfmtest/presenter/SecondActivity.java
@@ -1,11 +1,13 @@
 package com.example.tfmtest.presenter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,6 +24,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.example.tfmtest.model.Reporte;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
 
@@ -36,8 +45,8 @@ public class SecondActivity extends AppCompatActivity{
     List<Reporte> reportes;
     RecyclerView recyclerView;
     ReportsListAdapter itemAdapter;
-    DatabaseReference databaseReference;
-
+   // DatabaseReference databaseReference;
+    FirebaseFirestore firebaseFirestore;
 
 
     public static final int NEW_DERRUMBE_ACTIVITY_REQUEST_CODE = 1;
@@ -52,8 +61,6 @@ public class SecondActivity extends AppCompatActivity{
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
         signOutBtn = findViewById(R.id.signout_btn);
-        recyclerView = findViewById(R.id.recyclerview);
-
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(this,gso);
@@ -65,10 +72,6 @@ public class SecondActivity extends AppCompatActivity{
             name.setText(personName);
             email.setText(personEmail);
         }
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(new ReportsListAdapter(this));
 
             signOutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,8 +98,30 @@ public class SecondActivity extends AppCompatActivity{
             }
         });
 
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Query query = firebaseFirestore.collection("Reportes");
 
 
+        FirestoreRecyclerOptions<Reporte> firestoreRecyclerOptions = new FirestoreRecyclerOptions.Builder<Reporte>().setQuery(query, Reporte.class).build();
+
+        itemAdapter = new ReportsListAdapter(firestoreRecyclerOptions);
+        itemAdapter.notifyDataSetChanged();
+        recyclerView.setAdapter(itemAdapter);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        itemAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        itemAdapter.stopListening();
 
     }
 
